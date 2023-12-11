297cb20f309505eea63fccf2597b61a2d9494d90
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-11 23:18:54 +0400

    strat

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var btn = document.getElementById('btn');
+var btn = document.getElementById('btn');
+
+function increaseSize() {
+    btn.style.fontSize = '30px';
+    btn.style.transition ='0.6s';
+}
+
+function decreaseSize() {
+    btn.style.fontSize = 'inherit';
+    btn.style.transition ='0.6s';
+}
+
+
+
+
+
+  
